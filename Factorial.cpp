55a24ca2be1e386c84618dfab64c4c5b75da00cb
chhetri28 <chettrisourav28@gmail.com>
2020-02-04 23:16:33 +0530

    first

---
+++ Factorial.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int z=0;
+	    int c=5;
+	    while((n/c)>0){
+	        z=z+(n/c);
+	        c=c*5;
+	    }
+	    cout<<z<<endl;
+	}
+}
